7dec3e306fb6b68d496f8d9fd3f9d9ac2877117f
Mahdi <86263611+Mhdph@users.noreply.github.com>
2023-06-09 18:56:04 +0330

    add pocketbase to project

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,19 @@
+import type { User } from '$lib/types';
+import PocketBase, { Admin, Record } from 'pocketbase';
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+// and what to do when importing types
 declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
+	declare namespace App {
+		interface Locals {
+			pb: PocketBase;
+			user: User | undefined;
+		}
 		// interface PageData {}
+		// interface Error {}
 		// interface Platform {}
 	}
+	declare module 'simple-svelte-autocomplete';
 }
-
-export {};
+/// <reference types="@sveltejs/kit" />
+/// <reference types="unplugin-icons/types/svelte" />
